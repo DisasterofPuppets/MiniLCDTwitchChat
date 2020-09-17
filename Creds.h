ebf1f52631b8cf51c80a2262162c872c501e2739
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2020-09-17 13:35:47 +1000

    Create Creds.h

---
+++ Creds.h
@@ -0,0 +1,17 @@
+#define IRC_SERVER   "irc.chat.twitch.tv"
+#define IRC_PORT     6667
+
+#define WIFI_SSID "yourwifinetwork"
+#define WIFI_PASS "yourwifipassword"
+
+//The name of the channel that you want the bot to join
+const String twitchChannelName = "yourchannelname";
+
+//The name that you want the bot to have
+#define TWITCH_BOT_NAME "thebotname"
+
+//OAuth Key for your twitch bot
+// https://twitchapps.com/tmi/
+#define TWITCH_OAUTH_TOKEN "oauth:thismustbetheoauthmatchingthebot"
+//IE, if you are using another name beside your channel as the bot, you need to create a twitch
+//account with that name and place the oauth for that account here
